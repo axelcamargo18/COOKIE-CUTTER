54dc0344560ed3db7fce1b83330bd66537fc84da
Axel Camargo <axelcamargo@AdminisatorsMBP.lan>
2022-09-16 23:00:51 -0400

    Cookie Cut

---
+++ script.js
@@ -0,0 +1,22 @@
+// var bakeryWelcome = prompt("Welcome to the bakery ");
+
+document.write("Hope you like cookies");
+
+let clicks = 0;
+
+function increment() {
+  if (clicks >= 10) {
+    clicks += 10;
+    document.getElementById("level-info").innerText = "🔥 LEVEL UP!! 🔥";
+  } else {
+    console.log("Default value");
+    clicks++;
+  }
+
+  counter.innerHTML = clicks;
+}
+
+function reset() {
+  clicks = 0;
+  counter.innerHTML = clicks;
+}
